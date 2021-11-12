4b3eb6e290cdf2610bfcc3377744e8433d31ac0f
usmanyunusov <usman.iunusov@gmail.com>
2021-11-12 19:08:34 +0300

    Fixed git tests

--- git/index.test.js
+++ git/index.test.js
@@ -5,10 +5,11 @@ import { fileURLToPath } from 'url'
 import { equal } from 'uvu/assert'
 import { test } from 'uvu'
 
-import { execGit, gitWorker } from './index.js'
+import { execGit as baseExecGit, gitWorker } from './index.js'
 
 let currentDir = dirname(fileURLToPath(import.meta.url))
 let cwd = resolve(currentDir, '../test/fixtures/git/nano-staged-test')
+let patchPath = join(cwd, 'nano-staged.patch')
 
 async function appendFile(filename, content, dir = cwd) {
   await fs.appendFile(resolve(dir, filename), content)
@@ -18,21 +19,21 @@ async function writeFile(filename, content, dir = cwd) {
   await fs.writeFile(resolve(dir, filename), content)
 }
 
-async function makeDir() {
-  await fs.mkdir(cwd)
+async function makeDir(dir = cwd) {
+  await fs.mkdir(dir)
 }
 
-async function exGit(args) {
-  await execGit(args, { cwd })
+async function execGit(args) {
+  await baseExecGit(args, { cwd })
 }
 
 async function initGitRepo() {
-  await exGit(['init'])
-  await exGit(['config', 'user.name', '"test"'])
-  await exGit(['config', 'user.email', '"test@test.com"'])
+  await execGit(['init'])
+  await execGit(['config', 'user.name', '"test"'])
+  await execGit(['config', 'user.email', '"test@test.com"'])
   await appendFile('README.md', '# Test\n')
-  await exGit(['add', 'README.md'])
-  await exGit(['commit', '-m initial commit'])
+  await execGit(['add', 'README.md'])
+  await execGit(['commit', '-m initial commit'])
   await writeFile('README.md', '# Test\n## Test')
 }
 
@@ -42,7 +43,6 @@ await initGitRepo()
 test('gitWorker: diffPatch', async () => {
   let git = gitWorker({ cwd })
 
-  let patchPath = join(cwd, 'nano-staged.patch')
   await git.diffPatch(patchPath)
 
   let source = await fs.readFile(patchPath)
@@ -58,19 +58,40 @@ test('gitWorker: diffPatch', async () => {
   )
 })
 
-test('gitWorker: checkout, checkPatch, applyPatch', async () => {
+test('gitWorker: checkout', async () => {
   let git = gitWorker({ cwd })
 
-  let patchPath = join(cwd, 'nano-staged.patch')
   await git.checkout(['.'])
 
-  let source = await fs.readFile(join(cwd, 'README.md'))
-  equal(source.toString(), '# Test\n')
+  let files = await git.getStagedFiles({ gitDir: cwd, cwd })
+  equal(files, [])
+})
 
-  await git.checkPatch(patchPath)
+test('gitWorker: applyPatch', async () => {
+  let git = gitWorker({ cwd })
+
   await git.applyPatch(patchPath)
-  source = await fs.readFile(join(cwd, 'README.md'))
+
+  let source = await fs.readFile(join(cwd, 'README.md'))
   equal(source.toString(), '# Test\n## Test')
+})
+
+test('gitWorker: add', async () => {
+  let git = gitWorker({ cwd })
+
+  await git.add(['.'])
+
+  let files = await git.getStagedFiles({ gitDir: cwd, cwd })
+  equal(files.length, 2)
+})
+
+test('gitWorker: checkPatch', async () => {
+  let git = gitWorker({ cwd })
+
+  equal(await git.checkPatch(patchPath), true)
+
+  await writeFile(patchPath, '')
+  equal(await git.checkPatch(patchPath), false)
 })
 
 test('', async () => {
