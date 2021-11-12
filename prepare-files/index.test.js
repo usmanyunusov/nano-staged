eae97826835f3be4d84e604750d144279d1c196b
usmanyunusov <usman.iunusov@gmail.com>
2021-11-12 14:27:43 +0300

    Added prepare files test

---
+++ prepare-files/index.test.js
@@ -0,0 +1,34 @@
+import { equal } from 'uvu/assert'
+import { test } from 'uvu'
+
+import { prepareFiles } from './index.js'
+
+let config = { '*.{css,js}': ['prettier --write'] }
+let entries = [
+  { path: 'a.js', type: 1 },
+  { path: 'b.js', type: 2 },
+  { path: 'c.css', type: 1 },
+  { path: 'd.md', type: 4 },
+  { path: 'e.css', type: 4 },
+]
+
+test(`track prepareFiles on '*.{css,js}': ['prettier --write']`, () => {
+  let files = prepareFiles(entries, config)
+
+  equal(files, {
+    tasks: [
+      [
+        {
+          pattern: '*.{css,js}',
+          cmd: 'prettier --write',
+          files: ['a.js', 'b.js', 'c.css', 'e.css'],
+        },
+      ],
+    ],
+    staged: ['a.js', 'b.js', 'c.css', 'e.css'],
+    deleted: ['e.css'],
+    changed: ['b.js'],
+  })
+})
+
+test.run()
