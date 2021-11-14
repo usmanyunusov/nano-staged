583b76a196cfafa8c53477c23ea24cc5d678d7fa
Usman Yunusov <usman.iunusov@gmail.com>
2021-11-15 01:09:10 +0300

    Fixed fs test

--- fs/index.test.js
+++ fs/index.test.js
@@ -6,14 +6,19 @@ import { test } from 'uvu'
 
 import { fileSystem } from './index.js'
 
-let dir = dirname(fileURLToPath(import.meta.url))
+const DIRNAME = dirname(fileURLToPath(import.meta.url))
+
+function fixture(name) {
+  return resolve(DIRNAME, '../test/fixtures', name)
+}
+
 let example = [
   {
-    path: resolve(dir, '../test/fixtures/fs/a.js.test'),
+    path: fixture('fs/a.js.test'),
     source: 'let a = 1;',
   },
   {
-    path: resolve(dir, '../test/fixtures/fs/b.css.test'),
+    path: fixture('fs/b.css.test'),
     source: 'a {color: red;}',
   },
 ]
@@ -21,34 +26,29 @@ let example = [
 test('should write correctly files', async () => {
   let fs = fileSystem()
   await fs.write(example)
-
   let hasFiles = example.every(async ({ path }) => await baseFs.access(path))
+
   equal(hasFiles, true)
 })
 
 test('should read correctly files', async () => {
   let fs = fileSystem()
-  let sources = await fs.read([
-    resolve(dir, '../test/fixtures/fs/a.js'),
-    resolve(dir, '../test/fixtures/fs/b.css'),
-  ])
+  let sources = await fs.read([fixture('fs/a.js'), fixture('fs/b.css')])
+
   equal(sources.length, 2)
 })
 
 test('should delete correctly files', async () => {
   let fs = fileSystem()
   await fs.delete(example.map(({ path }) => path))
-
   let deleteFiles = await fs.read(example.map(({ path }) => path))
+
   equal(deleteFiles.length, 0)
 })
 
 test('should not read file', async () => {
   let fs = fileSystem()
-  let sources = await fs.read([
-    resolve(dir, '../test/fixtures/fs/a_null.js'),
-    resolve(dir, '../test/fixtures/fs/b_null.css'),
-  ])
+  let sources = await fs.read([fixture('fs/a_null.js'), fixture('fs/b_null.css')])
 
   equal(sources.length, 0)
 })
